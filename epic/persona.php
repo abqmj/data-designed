539f28e41ff035096dc6b628937ad8dd047d63fa
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 21:10:46 -0600

    worked on persona.. added image for an avatar(scale it down tomorrow).

---
+++ epic/persona.php
@@ -0,0 +1,21 @@
+<!
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<script type="text/javascript" src="java/js.js"></script>
+		<title>aRodgersLoveR69 autographsy profile page</title>
+	</head>
+	<body>
+		<h1>This is my very first persona!</h1>
+<p>This is the persona Gerrit. A HUGE Aaron Rodgers fan despite what the restraining order says he is no stalker. Gerrit's home state being Wisconsin have a love for the Green Bay Packers is in his blood. Since relocating out of his home state he loves wearing Packer gear every chance he gets. He also collects game worn jerseys the dirtier the more collectible, at least that is what he tells people.
+</p>
+		<h1>aRodgersLoveR69</h1>
+<ul>
+	<li>email: gerritpackersfan1111@netscape.net</li>
+	<li>Favorite sports Team: Green Bay Packers</li>
+	<li>location: New Mexico Originally from Wisconsin</li>
+	<li> avatar: <img src="images/gavatar.png" alt="GOAT"> </li>
+</ul>
+	</body>
+</html>
