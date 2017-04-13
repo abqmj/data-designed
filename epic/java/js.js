29389b66c2fb2abe51c8d7530c852ae7ac287987
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 19:36:47 -0600

    Got persona with the norm stuff. Also added in css / java for future use and created folders and files for such future use. Also added in title 'Use case for a very bad ETSY site'

---
+++ epic/java/js.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mikes on 4/12/2017.
+ */
---
+++ epic/use-case.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+	<!--did !doc tab in phpstorm to start im assuming this is not considered copy pasta because I don't cook-->
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/style.css"
+		type="text/css"/>
+		<script type="text/javascript" src="java/js.js"></script>
+		<!--added in css and java for future use also practice-->
+		<title>Use case for a very bad ETSY site</title>
+	</head>
+	<body>
+
+	</body>
+</html>
