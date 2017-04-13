05a96460092ea7d3d6596e004e2677f19021adcd
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 20:16:57 -0600

    put together usecase with headers ps and a list of what a user is expected to do when visiting the site.

--- epic/use-case.php
+++ epic/use-case.php
@@ -10,6 +10,16 @@
 		<title>Use case for a very bad ETSY site</title>
 	</head>
 	<body>
+		<h1>First a quick explanation of Autographsy</h1>
+		<p>Autographsy is a site for signed Memorabilia with an ETSY twist. In the future users would be able to view and purchase signed/autographed memorabilia from their favorite Sports stars, but also Celebrities.</p>
+		<p>Also in the future autographsy will offer user to client services allowing users to reach out to the professional fans(guys who get autographs for a living) for a specific autograph. For example if your a dog lover and owned a boxer you might want a dog collar signed by the greatest boxer ever Floyd Money Mayweather.</p>
+		<h1>Now onto the use-case for Autographsy(phase 0)</h1>
+		<p> For the time being users are limited to liking their favorite autographed memorabilia. So users can create an account and login into the site to browse the different memorabilia.</p>
+<ol>
+			<li>User Gerrit creates profile arodgerslover69 using an apple macbook covered in packers stickers</li>
+	<li>User Gerrit searches for game worn aaron rodgers jerseys</li>
+	<li>User Gerrit finds the dirtiest sweatiest looking jersey and likes it so he can purchase it in the future when sales go live</li>
 
+		</ol>
 	</body>
 </html>
