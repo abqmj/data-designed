e0e0f7581925cb0b1cc46a63b1758b75a887f160
Michael Jordan <mikesjordan@gmail.com>
2017-04-17 08:54:55 -0600

    comment about something im unsure of

--- sql/datadesigned.sql
+++ sql/datadesigned.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS 'favorite';
-DROP TABLE IF EXISTS 'product';
-DROP TABLE IF EXISTS 'profile';
+DROP TABLE IF EXISTS favorite;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS profile;
 
-CREATE TABLE profile(
+CREATE TABLE profile (
 profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 profileActivationToken CHAR(32),
 profileUsername VARCHAR(32) NOT NULL,
@@ -19,12 +19,12 @@ CREATE TABLE product (
 productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 productDescription VARCHAR(2000) UNIQUE NOT NULL,
 productPrice CHAR(10) UNIQUE NOT NULL,
--- productFavorite not sure on this table dam jeopardy
+-- productFavorite not sure on this if product is a tweet this would be likes but unsure how I would write this
 INDEX(productId),
 PRIMARY KEY(productId)
 );
 
-CREATE TABLE favorite(
+CREATE TABLE favorite (
 favoriteProfileId INT UNSIGNED NOT NULL,
 favoriteProductId INT UNSIGNED NOT NULL,
 INDEX(favoriteProfileId),
