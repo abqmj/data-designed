831d1b7fcb6670e1b5c465dc070093a5e10651b1
Michael Jordan <mikesjordan@gmail.com>
2017-04-20 10:31:11 -0600

    created autoload php hmb

---
+++ epic/php/classes/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This will be my very first autoloader HMB
+ *
+ * God willing it will load classes via the prefix and class name.
+ * @param string $class fully legit class name to load
+ * @see deepdivedylan@github also follow his twitter @deepdivedylan
+ * @see php-fig.org/psr/psr-4/examples
+ **/
